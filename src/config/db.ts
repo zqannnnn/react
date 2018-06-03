bc67c1d27c01bd1e3e36629668178cd8f421e4ce
lihua <lihua950411@gmail.com>
2018-06-03 13:58:52 +0800

    change UIFrame from bootstrap to antd(Component:Upload);

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,6 @@
 export const config = {
   username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'lihua403',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
   database: process.env.POSTGRES_DB || 'postgres',
   host: process.env.POSTGRES_HOST || 'postgres',
   dialect: 'postgres'
