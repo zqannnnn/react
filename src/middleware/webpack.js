4b9cba2ab3079dac827304c388fdfe804c62572e
chekst <israel.gavriel@gmail.com>
2018-06-20 16:16:04 +0800

    different configs per env

--- src/middleware/webpack.js
+++ src/middleware/webpack.js
@@ -1,7 +1,13 @@
-const config = require('../../webpack.config.js')
 const webpack = require('webpack')
 const webpackMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
+
+var config
+if ( process.env.NODE_ENV === 'production') {
+  config = require('../../webpack.prod.js')
+} else {
+  config = require('../../webpack.dev.js')
+}
 
 module.exports = app => {
   const compiler = webpack(config)
