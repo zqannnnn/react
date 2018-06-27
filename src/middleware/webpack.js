5bb078075d59c56232d90b4addaf7529eafc81de
chekst <israel.gavriel@gmail.com>
2018-06-27 20:56:34 +0800

    ci checking prod settings 1

--- src/middleware/webpack.js
+++ src/middleware/webpack.js
@@ -3,8 +3,8 @@ const webpackMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 
 //const config = require('../../webpack.config.js')
-const config = require('../../webpack.dev.js')
-//if ( process.env.NODE_ENV == 'production') config = require('../../webpack.prod.js')
+let config = require('../../webpack.dev.js')
+if ( process.env.NODE_ENV == 'production') config = require('../../webpack.prod.js')
 
 module.exports = app => {
   const compiler = webpack(config)
