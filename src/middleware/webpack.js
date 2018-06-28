6cb975cb16b3ef0dd95249c5dce9d258673672ae
chekst <israel.gavriel@gmail.com>
2018-06-28 20:12:41 +0800

    remove old webpack config from deploy

--- src/middleware/webpack.js
+++ src/middleware/webpack.js
@@ -2,7 +2,6 @@ const webpack = require('webpack')
 const webpackMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 
-//const config = require('../../webpack.config.js')
 let config = require('../../webpack.dev.js')
 if ( process.env.NODE_ENV == 'production') config = require('../../webpack.prod.js')
 
