d3e1921c601746c7ef7d6a462e96ef0fe5b6be21
Louis Liu <liukangqi56@hotmail.com>
2018-06-29 09:52:04 +0800

    refactor filter compoent

--- frontend/src/components/filter.tsx
+++ frontend/src/components/filter.tsx
@@ -4,6 +4,7 @@ import { transactionConsts } from '../constants'
 import { RootState, CurrencyState } from '../reducers'
 import { Row, Col, Checkbox, Select } from 'antd'
 import { currencyActionCreators } from '../actions'
+import { ListOptions } from '../models'
 import i18n from 'i18next'
 
 const Option = Select.Option
@@ -11,28 +12,58 @@ const Option = Select.Option
 interface ItemProps {
   dispatch: Dispatch<RootState>
   currency: CurrencyState
-  handleSelectSort: (value: string) => void
-
-  handleChangeType: (values: string[]) => void
+  onOptionsChange: (option: ListOptions) => void
+  initOptions: ListOptions
+}
+interface ItemState {
+  options: ListOptions
 }
 
-class Filter extends React.Component<ItemProps> {
+class Filter extends React.Component<ItemProps, ItemState> {
   constructor(props: ItemProps) {
     super(props)
+    this.state = { options: {} }
   }
-
+  componentDidMount() {
+    this.setState({ options: this.props.initOptions })
+  }
   handleSelectCurrency = (value: string) => {
     this.props.dispatch(currencyActionCreators.upCurrencystatus(value))
   }
-
+  handleChangeType = (values: string[]) => {
+    let newOptions = this.state.options
+    if (values.length === 2) {
+      newOptions.buy = true
+      newOptions.sell = true
+    } else if (values.length === 1) {
+      if (values[0] === transactionConsts.TYPE_BUY) {
+        newOptions.buy = true
+        newOptions.sell = false
+      } else {
+        newOptions.buy = false
+        newOptions.sell = true
+      }
+    } else if (values.length === 0) {
+      newOptions.buy = false
+      newOptions.sell = false
+    }
+    this.setState({ options: newOptions })
+    this.props.onOptionsChange(newOptions)
+  }
+  handleSelectSort = (value: string) => {
+    let options = this.state.options
+    options.sorting = value
+    this.setState({ options })
+    this.props.onOptionsChange(options)
+  }
   render() {
-    const { currency, handleChangeType, handleSelectSort } = this.props
+    const { currency } = this.props
     return (
       <div className="filter margin-bottom">
         <Row>
           <Select
             defaultValue={i18n.t('New to old')}
-            onChange={handleSelectSort}
+            onChange={this.handleSelectSort}
             className="sorting-left margin-bottom media-width"
           >
             <Option value="new">{i18n.t('New to old')}</Option>
@@ -40,7 +71,7 @@ class Filter extends React.Component<ItemProps> {
           </Select>
           <div className="float-right">
             <Checkbox.Group
-              onChange={handleChangeType}
+              onChange={this.handleChangeType}
               className="margin-bottom"
             >
               <Checkbox
