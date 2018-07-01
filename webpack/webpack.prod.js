04dd0163cc5cecccada78cfce7057c5889a43937
chekst <israel.gavriel@gmail.com>
2018-07-01 18:07:50 +0800

    minify css in prod

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,12 +2,16 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const webpack = require('webpack')
 
 module.exports = merge(common, {
     devtool: 'source-map',
     plugins: [
         new UglifyJSPlugin({
             sourceMap: true
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
         })
-    ]  
+      ]   
 });
