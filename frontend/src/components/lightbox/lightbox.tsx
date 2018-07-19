ece15b51fd10cd27c1f061b81c40e37c5e34fa2c
Louis Liu <liukangqi56@hotmail.com>
2018-07-19 16:45:12 +0800

    reorganize lightbox component

--- frontend/src/components/lightbox/lightbox.tsx
+++ frontend/src/components/lightbox/lightbox.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { connect, Dispatch } from 'react-redux'
-import { lightboxActionCreators } from '../actions'
-import { RootState, LightboxState } from '../reducers'
+import { lightboxActionCreators } from '../../actions'
+import { RootState, LightboxState } from '../../reducers'
 import { Icon } from 'antd'
 import './lightbox.scss'
 
