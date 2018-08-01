4d71e9ab529005b0e2ed2736b98ed16dd20f5466
chekst <israel.gavriel@gmail.com>
2018-08-01 17:52:47 +0700

    get online users

--- frontend/src/components/chat/chat.tsx
+++ frontend/src/components/chat/chat.tsx
@@ -7,58 +7,29 @@ import { AuthState } from '../../reducers'
 //https://github.com/ant-design/ant-design/blob/master/components/collapse/demo/accordion.md
 import { Collapse } from 'antd'
 import './chat.scss'
-//import Collapse from 'rc-collapse'
-//import { AuthInfo } from '../actions'
-//import { Currency } from '../models'
 interface ItemProps {
   auth: AuthState
-  //dispatch: Dispatch<RootState>
-  //price: number
-  //currencyCode: string
-  //currencyState: CurrencyState
 }
 class Chat extends React.Component<ItemProps> {
   constructor(props: ItemProps) {
     super(props)
-    //1532692062 chat
     //this.state = {
     //  endpoint: "http://localhost:3000" // this is where we are connecting to with sockets
     //}
   }
   render() {
-    //console.log(AuthState)
     let { auth } = this.props
     const { loggedIn, authInfo } = auth
-    //console.log('!!!!!!!!!!!!!!!!!')
-    //console.log(authInfo)
-    //const { loggedIn, authInfo } = auth
-    //console.log('loggedIn: ' + loggedIn)
-    //1532692062 chat
-    // Within the render method, we will be checking for any sockets.
-    // We do it in the render method because it is ran very often.
-    //1532692062 chat
-    //const socket = socketIOClient(this.state.endpoint)
     const socket = socketIOClient("http://localhost:3000")    
-    //socket.set('nickname', authInfo['username']);
-    //socket.nickname = authInfo['username'];
     socket.on('connect', function () {
       if ( authInfo !== undefined && authInfo['email'] !== undefined ) {
-        console.log('!!!!!!!!!! send nickname')
-        console.log(authInfo)
-        //console.log(authInfo['username'])
-        socket.emit('set email', authInfo['email']);
+        socket.emit('get-users', authInfo['email']) 
       }
-      //socket.on('ready', function () {
-      //  console.log('Connected !');
-      //  socket.emit('msg', 'Static Message'));
-      //});
+      socket.on('get-users', (users: any) => {
+        console.log(users)
+      })      
     });    
-    socket.emit('get-users', '') 
 
-    //var clients = socket.clients();
-    //console.log(clients)
-    //const { price, currencyCode } = this.props
-    //let newPrice = this.exchangeCurrency(price, currencyCode)
     const Panel = Collapse.Panel
     let chat: JSX.Element
     if (loggedIn) {
@@ -85,6 +56,5 @@ class Chat extends React.Component<ItemProps> {
   }
 }
 
-//const connectedItem = connect(Chat)
 export { Chat }
 
--- src/index.ts
+++ src/index.ts
@@ -59,6 +59,8 @@ const server = app.listen(port, (err: string) => {
     `⚡ Express started on port ${port}, in ${process.env.NODE_ENV} mode`
   )
 })
+
+
 //1532692062 chat
 const io = socket(server)
 export interface IHash {
@@ -66,57 +68,16 @@ export interface IHash {
 }
 let users: IHash = {};
 io.sockets.on('connection', function (socket) {
-  socket.on('set email', function (email) {
-    console.log('!!!!!!!!!! set nickname')
-    //console.log(name)
-    //console.log(socket.id)
-    users[socket.id] = email
-    //console.log(socket.nsp.sockets[socket.id])
-    //console.log(Object.keys(socket.nsp.sockets));
-    //io.sockets.sockets['nickname'] = name;
-    //socket.set('nickname', name, function () { socket.emit('ready'); })
-  })
 })
-/*
-io.sockets.on('connection',function(socket){ 
-  io.sockets.sockets['nickname'] = socket.id;
-  client.on("chat", function(data) {      
-      var sock_id = io.sockets.sockets['nickname']
-      io.sockets.sockets[sock_id].emit("private", "message");
-  });    
-});
-*/
 io.on('connection', socket => {
-  console.log('New client connected')
-  
-  // just like on the client side, we have a socket.on method that takes a callback function
-  socket.on('change color', (color) => {
-    // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
-    // we make use of the socket.emit method again with the argument given to use from the callback function above
-    console.log('Color Changed to: ', color)
-    io.sockets.emit('change color', color)
-  })
-  /*
-  socket.on('set nickname', function (name) {
-    socket.set('nickname', name, function () { socket.emit('ready'); });
-  });  
-  */
-  socket.on('get-users', (nothing) => {
-    // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
-    // we make use of the socket.emit method again with the argument given to use from the callback function above
-    //let clients = io.sockets.clients()
-    console.log(users)
-    io.sockets.clients((error :any, clients :any) => {
-      //if (error) throw error;
-      //console.log(clients); // => [PZDoMHjiu8PYfRiKAAAF, Anw2LatarvGVVXEIAAAD]
-    });    
-    //console.log('Users: ', clients)
-    //io.sockets.emit('get-users', clients)
-  })
-  
-  // disconnect is fired when a client leaves the server
+  socket.on('get-users', (email) => {
+    //io.sockets.clients((error :any, clients :any) => {
+    //});    
+    users[socket.id] = email
+    io.sockets.emit('get-users', users)
+  })  
   socket.on('disconnect', () => {
-    console.log('user disconnected')
+    delete users[socket.id]
   })
 })
 
