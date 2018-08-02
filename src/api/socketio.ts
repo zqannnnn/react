d43b7c8a3dd693cf8500af9d25773625c31e52d8
chekst <israel.gavriel@gmail.com>
2018-08-02 10:47:11 +0700

    updated socketio clients list send to client

--- src/api/socketio.ts
+++ src/api/socketio.ts
@@ -2,8 +2,11 @@ import * as socket from 'socket.io' //1532692062 chat
 import { AuthInfo } from '../../frontend/src/actions'
 //https://socket.io/get-started/chat/
 //https://codeburst.io/isomorphic-web-app-react-js-express-socket-io-e2f03a469cd3
+interface SHash {
+    [details: string] : any;
+}
 interface IHash {
-    [details: string] : AuthInfo;
+  [details: string] : SHash;
 }
 const startSocket = async (server: any) => {
     const io = socket(server)
@@ -12,9 +15,31 @@ const startSocket = async (server: any) => {
     })
     io.on('connection', socket => {
       socket.on('get-users', (authInfo: AuthInfo) => {
-        //io.sockets.clients((error :any, clients :any) => {
-        //});    
-        users[socket.id] = authInfo
+        /*
+        io.sockets.clients((error :any, clients :any) => {
+          console.log('clients')
+          console.log(clients)
+        });    
+        */
+        let aInfo: SHash = {};
+        aInfo['id'] = authInfo.id
+        aInfo['name'] = authInfo.name
+        aInfo['ts'] = Date.now()
+        users[socket.id] = aInfo
+        let newUsers: IHash = {};
+        for ( var key in users ) {
+          let paste = true
+          for ( var key1 in newUsers ) {
+            if ( newUsers[key1]['id'] == users[key]['id'] && newUsers[key1]['ts'] > users[key]['ts'] ) {
+              paste = false
+            }
+            if ( newUsers[key1]['id'] == users[key]['id'] && newUsers[key1]['ts'] < users[key]['ts'] ) {
+              delete newUsers[key1]
+            }
+          }
+          if (paste) newUsers[key] = users[key]
+        }
+        users = newUsers
         io.sockets.emit('get-users', users)
       })  
       socket.on('disconnect', () => {
