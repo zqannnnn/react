1bcf37bf0e0e6528ccc4aa71e26e226fc7ee4e8a
chekst <israel.gavriel@gmail.com>
2018-08-12 16:52:04 +0700

    refact

--- frontend/src/components/chat/user-item.tsx
+++ frontend/src/components/chat/user-item.tsx
@@ -1,9 +1,16 @@
+
 //1532692062 chat
+
+//https://ant.design/components/collapse/
+//https://github.com/ant-design/ant-design/blob/master/components/collapse/demo/accordion.md
+import { Collapse } from 'antd'
+
 import * as React from 'react'
 import { StringKeyHash } from '../../../../src/interfaces'
 
 interface ItemProps {
     user: StringKeyHash
+    key: string
 }
 class UserItem extends React.Component<ItemProps> {
 	constructor(props: ItemProps) {
@@ -13,9 +20,12 @@ class UserItem extends React.Component<ItemProps> {
 	}
 	render() {
         let chatItem: JSX.Element
+		const Panel = Collapse.Panel
         chatItem = (
             <>
-                <h2>{this.props.user.name}</h2>
+                <Panel header={this.props.user.name} key={this.props.key}>
+                    <h2>{this.props.user.name}</h2>
+                </Panel>
             </>
         )
 		return chatItem
