9b80d8e3964b52c61ad5bacf3e7a6a665b9b6643
chekst <israel.gavriel@gmail.com>
2018-09-10 11:27:02 +0800

    socket tests

---
+++ tests/api/socketio.test.ts
@@ -0,0 +1,22 @@
+//import * as socketIOClient from 'socket.io-client'
+/*
+import * as socket from 'socket.io' 
+const socketIO = require('../../src/api/socketio')
+
+jest.mock('socket.io', () => {
+    return {
+        connect: jest.fn(() => {
+            return {
+                on: jest.fn()
+            }
+        })
+    }
+})
+
+
+describe('SocketIO', () => {
+    it('logged-out user', () => {
+        socketIO.startSocket()
+    })
+})
+*/
