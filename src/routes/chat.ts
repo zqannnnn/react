6428c225b3c6063a139a7fd2e533022aa416e850
chekst <israel.gavriel@gmail.com>
2018-09-13 14:49:55 +0800

    tslint fixes

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,6 +1,5 @@
-//1532692062 chat
+// 1532692062 chat
 import * as express from 'express'
-import * as i18n from 'i18next'
 import { authMiddleware, loginCheckMiddleware } from '../middleware/auth'
 import { IRequest } from '../middleware/auth'
 import { User, Message } from '../models/'
@@ -11,58 +10,57 @@ import { StringKeyHash } from '../interfaces'
 router.use(authMiddleware)
 
 router.get('/users', async (req: IRequest, res: express.Response) => {
-    const type = req.query.type
-    try {
-        const findAllTo = Message.findAll({
-            attributes: [
-                'to', 
-                sequelize.fn('max', sequelize.col('created_at'))
-            ],
-            group: ['to'],
-            where: {
-                from: req.userId,
-            },
-            raw: true,
-        })
-        let allUsers: StringKeyHash = {};
-        const findAllFrom = Message.findAll({
-            attributes: [
-                'from', 
-                sequelize.fn('max', sequelize.col('created_at'))
-            ],
-            group: ['from'],
-            where: {
-                to: req.userId,
-            },
-            raw: true,
-        })
-        const allMsgs = [await findAllTo, await findAllFrom]
-        allMsgs[0].forEach(function (msg, index) {
-            if (allUsers[msg.to] === undefined) allUsers[msg.to] = msg.max
-            if (allUsers[msg.to] !== undefined && allUsers[msg.to] < msg.max) allUsers[msg.to] = msg.max
-        });
-        allMsgs[1].forEach(function (msg, index) {
-            if (allUsers[msg.from] === undefined) allUsers[msg.from] = msg.max
-            if (allUsers[msg.from] !== undefined && allUsers[msg.from] < msg.max) allUsers[msg.from] = msg.max
-        });
-        var allUsersSorted: StringKeyHash = {};
-        var userIds = Object.keys(allUsers);
-        userIds.sort(function(a, b) {
-          return allUsers[a] - allUsers[b]
-        }).reverse().forEach(function(k) {
-            allUsersSorted[k] = allUsers[k];
-        });
-        const users = await User.findAll({
-            attributes: { exclude: ['password', 'email'] },
-            where: {
-                id: userIds,
-            },
-            raw: true,
-        })
-        return res.send({ users: users, ids: userIds })
-    } catch (e) {
-        return res.status(500).send({ error: e.message })
-    }
+  // const type = req.query.type
+  try {
+    const findAllTo = Message.findAll({
+      attributes: ['to', sequelize.fn('max', sequelize.col('created_at'))],
+      group: ['to'],
+      where: {
+        from: req.userId
+      },
+      raw: true
+    })
+    const allUsers: StringKeyHash = {}
+    const findAllFrom = Message.findAll({
+      attributes: ['from', sequelize.fn('max', sequelize.col('created_at'))],
+      group: ['from'],
+      where: {
+        to: req.userId
+      },
+      raw: true
+    })
+    const allMsgs = [await findAllTo, await findAllFrom]
+    allMsgs[0].forEach((msg, index) => {
+      if (allUsers[msg.to] === undefined) allUsers[msg.to] = msg.max
+      if (allUsers[msg.to] !== undefined && allUsers[msg.to] < msg.max)
+        allUsers[msg.to] = msg.max
+    })
+    allMsgs[1].forEach((msg, index) => {
+      if (allUsers[msg.from] === undefined) allUsers[msg.from] = msg.max
+      if (allUsers[msg.from] !== undefined && allUsers[msg.from] < msg.max)
+        allUsers[msg.from] = msg.max
+    })
+    const allUsersSorted: StringKeyHash = {}
+    const userIds = Object.keys(allUsers)
+    userIds
+      .sort((a, b) => {
+        return allUsers[a] - allUsers[b]
+      })
+      .reverse()
+      .forEach(k => {
+        allUsersSorted[k] = allUsers[k]
+      })
+    const users = await User.findAll({
+      attributes: { exclude: ['password', 'email'] },
+      where: {
+        id: userIds
+      },
+      raw: true
+    })
+    return res.send({ users, ids: userIds })
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
 router.use(loginCheckMiddleware)
